62df20588496ef35532b56ca59989fa66bb70816
Nonso B-Anyeji <nonsobobanyeji@outlook.com>
2021-03-06 23:47:11 +0100

    Implementation of Menu Unit Test and Refactoring of Menu Komponent

--- src/Menu.js
+++ src/Menu.js
@@ -9,28 +9,60 @@
 import React, {Component} from 'react'
 
 // Export Component
-const Menu = () => {
-    return(
-	<div className="Menu">
-	    <nav className="megaMenu" role="navigation">
+class Menu extends Component{
 
-		{/* Mega Menu Controls */}
+    // State Initialisation
+    state = {
+	navMegaMenu: null,
+	btnExpand: null,
+	spanTitle: null,
+    }
+    
+    render(){
+	// Initialisation of Menu Button Click Event
+	const onClickExpand = () => {
+	    // Assign State Variables
+	    this.navMegaMenu = document.getElementById('navMegaMenu');
+	    this.btnExpand = document.getElementById('btnExpand');
+	    this.spanTitle = document.getElementById('spanTitle');
+	    console.log('btnExpand pressed');
+
+	    // Conditional Open and Close Statemnet for Expanding Mega Menu
+	    if(this.btnExpand.getAttribute('aria-expanded') === 'false'){
+		this.navMegaMenu.classList.add('megaMenuExpanded');
+		this.btnExpand.setAttribute('aria-expanded', 'true');
+		this.spanTitle.classList.remove('hamburgerTitleVisible');
+	    }else{
+		this.navMegaMenu.classList.remove('megaMenuExpanded');
+		this.btnExpand.setAttribute('aria-expanded', 'false');
+		this.spanTitle.classList.add('hamburgerTitleVisible');
+	    }
+	}
+	// Initialisation of Mega Menu Controls
+	const MegaMenuControls = () => {
+	    return(
 		<div className="megaMenuControls">
-		    <button className="megaMenuButton hamburger hamburgeElastic"
+		    <button id="btnExpand"
+			    className="megaMenuButton hamburger hamburgerElastic"
 			    type="button"
 			    aria-label="Menu"
 			    aria-expanded="false"
-			    aria-controls="primary-menu">
+			    aria-controls="primary-menu"
+			    onClick={()=>onClickExpand()}>
 			<span className="hamburgerBox">
 			    <span className="hamburgerInner"></span>
-			    <span className="hamburgerLabel"></span>
+			    <span className="hamburgerLabel">Menü</span>
 			</span>
-			<span className="hamburgerTitle hamburgerTitleVisible">Menu</span>
+			<span id="spanTitle" className="hamburgerTitle hamburgerTitleVisible">Menü</span>
 			<span className="hamburgerClose">Schließen</span>
 		    </button>
 		</div>
+	    );
+	}
 
-		{/* Primary Menu Component */}
+	// Initialisation of Mega Menu Primary Components
+	const MegaMenuComponents = () => {
+	    return(
 		<div id="primaryMenu" className="megaMenuRows">
 		    <div className="megaMenuRow megaMenuRow1">
 		    </div>
@@ -60,10 +92,28 @@ const Menu = () => {
 		    <div className="megaMenuRow megaMenuRow4">
 		    </div>
 		</div>
-		
-	    </nav>
-	</div>
-    );
+	    );
+	}
+	
+	// Initialisation of MegaMenu Navigation 
+	const MegaMenu = () => {
+	    return(
+		<nav id="navMegaMenu" className="megaMenu" role="navigation">
+		    {/* Mega Menu Controls */}
+		    <MegaMenuControls />
+		    {/* Primary Menu Component */}
+		    <MegaMenuComponents />
+		</nav>
+	    );
+	}
+
+	// Initialisation of Final Menu Export
+	return(
+	    <div className="Menu">
+		<MegaMenu />
+	    </div>
+	)
+    }
 }
 
-export default Menu;
+    export default Menu;
